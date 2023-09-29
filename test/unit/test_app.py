9811de9b84dac3dcea29e29b0b2478ee26291860
Max Anderson <maxwell.c.anderson@gmail.com>
2023-09-29 11:43:12 -0700

    test: Add unit tests for ralph endpoint

--- test/unit/test_app.py
+++ test/unit/test_app.py
@@ -1,6 +1,6 @@
 import unittest
 
-from app.app import hello_world, about_me
+from app.app import hello_world, about_me, ralph
 
 
 class AppTestCase(unittest.TestCase):
@@ -9,3 +9,11 @@ class AppTestCase(unittest.TestCase):
     def test_hello_world_endpoint(self):
         """Test the hello world endpoint returns a string."""
         assert isinstance(hello_world(), str)
+
+    def test_ralph_endpoint(self):
+        """Test the ralph endpoint returns a string."""
+        assert isinstance(ralph(), str)
+
+    def test_ralph_endpoint_returns_correct_string(self):
+        """Test the ralph endpoint returns the correct string."""
+        assert ralph() == "<p>Hi Ralph!</p>"
